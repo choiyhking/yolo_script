e4b7ac54531ca6ce6645a2ba44b3063dc04b022e
junhyukkkkk <158697328+junhyukkkkk@users.noreply.github.com>
2024-04-03 15:56:25 +0900

    for문 제거
    각 task마다 10번씩 실행하는 for문 제거함.
    고로 각 task마다 한 번씩 실행함.
    -> 대신 한 번 실행할 때, 각 이미지마다 inference time 각각 뽑아내서 저장하는 shell 구문 추가해야함.

--- predict_run.sh
+++ predict_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#predict
 declare -A command_model_map
 command_model_map["detect"]="yolov8n.pt"
 command_model_map["segment"]="yolov8n-seg.pt"
@@ -9,9 +9,9 @@ command_model_map["pose"]="yolov8n-pose.pt"
 for cmd in "${!command_model_map[@]}"; do
     model=${command_model_map[$cmd]}
     pre_log_file="predict_$cmd.log"
-    for ((i=1; i <= 10; i++)); do
         yolo $cmd predict model=$model source='/home/junhyuk/images/images/' | awk '/inference/{print $4}' >> $pre_log_file
-        echo "$i is completed for $model!"
-    done
+        echo "$cmd is completed for $model!"
 done
-#test
+# 각 awk 구문 각 image마다 inference time 각각 뽑아내는 구문으로 고쳐야함.
+# 각 pc 환경마다 data 경로 잘 파악하고 source 바꾸기.
+
