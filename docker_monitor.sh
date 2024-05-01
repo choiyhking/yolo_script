d3b36a4a224cd9260945af86ab2b1d03c5fedf28
IoT Research <161846673+yunhachoi@users.noreply.github.com>
2024-05-01 19:36:57 +0900

    Add files via upload

---
+++ docker_monitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# CPU usage monitoring script for yolov8 
+# Usage: ./monitor.sh <yolo task>
+#
+
+
+output_file="dokcer_cpu_$1"
+count=0
+
+while true; do
+    let count+=1
+     
+    if ! sudo docker exec $container_name ps aux | grep -q [y]olo; then
+	    break;
+    fi
+   
+    usage=$(sudo docker stats --no-stream $container_name | awk '{if(NR>1) {gsub(/%/, "", $3); print $3}}')
+    echo "$count $usage" >> ./cpu_results/"$output_file"
+done
+
+echo "<CPU statistics>"
+cpu_stats=$(python3 cal_cpu.py ./cpu_results/${output_file})
+echo "$cpu_stats" >> ./cpu_results/"$output_file"
+echo "$cpu_stats"
+echo ""
---
+++ docker_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Usage: ./docker_run.sh
+#
+
+container_name="yolo-docker"
+
+run_yolo_script() {
+  sudo docker exec $container_name /bin/bash -c "$SCRIPT_PATH/smallrun.sh $1" &
+  sleep 1
+  ./monitor.sh $1 # this script will finish when yolo process finished
+  echo "Stop container..."
+  sudo docker stop $container_name >/dev/null 2>&1
+}
+
+rm cpu_results/docker*
+
+SCRIPT_PATH=/usr/src/ultralytics/yolo_script
+INF_RESULT_PATH=$SCRIPT_PATH/inference_results
+INF_RESULT_FILENAME=$INF_RESULT_PATH/docker_inf_$1
+
+sudo docker start $container_name >/dev/null 2>&1
+sudo docker exec $container_name /bin/bash -c "mkdir $INF_RESULT_PATH; rm -rf $INF_RESULT_PATH/*"
+sudo docker stop $container_name >/dev/null 2>&1
+
+modes=("classify" "detect" "pose" "segment")
+
+for mode in "${modes[@]}"; do
+  echo "Start container..."
+  sudo docker start $container_name >/dev/null 2>&1
+  echo $mode
+  run_yolo_script "$mode"
+  sleep 5
+done
