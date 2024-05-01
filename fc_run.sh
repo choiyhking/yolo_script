8ea41a801d6a6f16839fb79fdf2878ce15d2bd27
IoT Research <161846673+yunhachoi@users.noreply.github.com>
2024-05-02 04:59:28 +0900

    Update fc_run.sh

--- fc_run.sh
+++ fc_run.sh
@@ -8,11 +8,11 @@ rm cpu_results/fc*
 
 modes=("classify" "detect" "pose" "segment")
 
-ssh -i ubuntu-22.04.id_rsa root@127.16.0.2 'cd /yolo_script/; mkdir inference_results; rm inference_results/fc*'
+ssh -i ubuntu-22.04.id_rsa root@172.16.0.2 'cd /yolo_script/; mkdir inference_results; rm inference_results/fc*'
 
 for mode in "${modes[@]}"; do
     echo "Start [[${mode}]]..."
-    ssh -i ubuntu-22.04.id_rsa root@127.16.0.2 '/yolo_script/smallrun.sh $1' &
+    ssh -i ubuntu-22.04.id_rsa root@172.16.0.2 '/yolo_script/smallrun.sh $1' &
     ./fc_monitor.sh $mode
     echo "completed!"
 
