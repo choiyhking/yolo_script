76e4cc4e7f5e160664edfa87589857f7e967d1a3
IoT Research <161846673+yunhachoi@users.noreply.github.com>
2024-05-01 22:24:21 +0900

    Add files via upload

---
+++ kvm_monitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# CPU usage monitoring script for yolov8 
+# Usage: ./kvm_monitor.sh <yolo task>
+#
+
+output_file="kvm_cpu_$1"
+count=0
+
+while true; do
+    let count+=1
+    
+    usage=$(top -b -n 1 | grep qemu-system-aar | head -n 1 | awk '{print $9}')
+
+    if ! ssh yolo@192.168.122.225 'ps aux | grep [y]olo'; then
+      break;
+    fi
+
+    echo "$count $usage" >> ./cpu_results/"$output_file"
+done
+
+echo "<CPU statistics>"
+cpu_stats=$(python3 cal_cpu.py ./cpu_results/${output_file})
+echo "$cpu_stats" >> ./cpu_results/"$output_file"
+echo "$cpu_stats"
+echo ""
---
+++ kvm_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Usage: ./kvm_run.sh
+#
+
+mkdir cpu_results
+rm cpu_results/kvm*
+
+modes=("classify" "detect" "pose" "segment")
+
+for mode in "${modes[@]}"; do
+    echo "Start [[${mode}]]..."
+    ssh yolo@192.168.122.225 '/yolo_script/smallrun.sh $1' &
+    ./kvm_monitor.sh $mode
+    echo "completed!"
+
+    sleep 10   
+done
+
+echo "All tasks finished successfully!!"
+
