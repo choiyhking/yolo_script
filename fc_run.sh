7a0a1a5aedf51c4f00af7b80ed3252201f2175a4
IoT Research <161846673+yunhachoi@users.noreply.github.com>
2024-05-31 21:30:16 +0900

    Update fc_run.sh

--- fc_run.sh
+++ fc_run.sh
@@ -10,7 +10,7 @@ tasks=("classify" "detect" "pose" "segment" "obb")
 
 ssh -i ubuntu-22.04.id_rsa root@172.16.0.2 "cd /yolo_script/; mkdir inference_results; rm inference_results/*"
 
-for mode in "${tasks[@]}"; do
+for task in "${tasks[@]}"; do
     echo "Start [[${task}]]..."
     ssh -i ubuntu-22.04.id_rsa root@172.16.0.2 "/yolo_script/smallrun.sh fc ${task}" &
     ./monitor.sh fc ${task}
