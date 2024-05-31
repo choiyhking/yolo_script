34e735015644de15fd4ddd7954ac6c6b1ee94acb
IoT Research <161846673+yunhachoi@users.noreply.github.com>
2024-05-31 16:04:36 +0900

    Update kata_run.sh

--- kata_run.sh
+++ kata_run.sh
@@ -12,7 +12,7 @@ tasks=("classify" "detect" "pose" "segment" "obb")
 
 for task in "${tasks[@]}"; do
     echo "Start [[${task}]]..."
-    ssh root@127.16.0.2 "/yolo_script/smallrun.sh ${task}" &
+    ssh root@127.16.0.2 "/yolo_script/smallrun.sh kata ${task}" &
     ./kata_monitor.sh ${task}
     echo "completed!"
 
