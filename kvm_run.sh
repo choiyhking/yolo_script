306ead9abb1cfb5dd93490422ad48f209790fa5e
IoT Research <161846673+yunhachoi@users.noreply.github.com>
2024-06-01 20:14:52 +0900

    Update kvm_run.sh

--- kvm_run.sh
+++ kvm_run.sh
@@ -3,19 +3,18 @@
 # Usage: ./kvm_run.sh
 #
 
-mkdir cpu_results
-rm cpu_results/kvm*
+mkdir cpu_results 2>/dev/null
+rm cpu_results/kvm* 2>/dev/null
 
 tasks=("classify" "detect" "pose" "segment" "obb")
 
-sshpass -p '1' ssh -o 'StrictHostKeyChecking=no' test@192.168.122.239 "cd ~/yolo_script/; mkdir inference_results; rm inference_results/kvm*"
+sshpass -p '1' ssh -o 'StrictHostKeyChecking=no' test@192.168.122.239 "cd ~/yolo_script/; mkdir inference_results 2>/dev/null; rm inference_results/kvm* 2>/dev/null"
 
 for task in "${tasks[@]}"; do
-    echo "Start [[${task}]]..."
-    sshpass -p '1' ssh -o 'StrictHostKeyChecking=no' test@192.168.122.239 "export PATH=/home/test/.local/bin:$PATH; cd ~/yolo_script; ./smallrun.sh kvm ${task}" &
-    ./monitor.sh kvm ${task}
-    echo "completed!"
-
+    echo "Start [[${task}]]"
+    ./monitor.sh kvm ${task} &
+    sshpass -p '1' ssh -o 'StrictHostKeyChecking=no' test@192.168.122.239 "export PATH=/home/test/.local/bin:$PATH; cd ~/yolo_script; ./smallrun.sh kvm ${task}"
+    
     sleep 10   
 done
 
