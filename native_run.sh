2abdc893d73b1ea15560ebbf2a46ccdfde2f1c36
IoT Research <161846673+yunhachoi@users.noreply.github.com>
2024-06-01 20:58:56 +0900

    Update native_run.sh

--- native_run.sh
+++ native_run.sh
@@ -6,7 +6,9 @@
 mkdir inference_results cpu_results 2>/dev/null
 rm inference_results/native* cpu_results/native* 2>/dev/null
 
-for task in "${!model_map[@]}"; do
+tasks=("classify" "detect" "pose" "segment" "obb")
+
+for task in "${tasks[@]}"; do
     echo "Start [[$task]]"
     ./monitor.sh native ${task} &
     ./smallrun.sh native ${task}
