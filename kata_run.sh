b114facbd7cf1a9cefbacaff6d9e4597a69e7b30
IoT Research <161846673+yunhachoi@users.noreply.github.com>
2024-06-01 20:28:17 +0900

    Update kata_run.sh

--- kata_run.sh
+++ kata_run.sh
@@ -3,18 +3,17 @@
 # Usage: ./kata_run.sh
 #
 
-mkdir cpu_results
-rm cpu_results/kata*
+mkdir cpu_results 2>/dev/null
+rm cpu_results/kata* 2>/dev/null
 
-ssh root@172.16.0.2 "cd /yolo_script/; mkdir inference_results; rm inference_results/*"
+ssh root@172.16.0.2 "cd /yolo_script/; mkdir inference_results 2>/dev/null; rm inference_results/* 2>/dev/null"
 
 tasks=("classify" "detect" "pose" "segment" "obb")
 
 for task in "${tasks[@]}"; do
-    echo "Start [[${task}]]..."
-    ssh root@127.16.0.2 "/yolo_script/smallrun.sh kata ${task}" &
-    ./kata_monitor.sh ${task}
-    echo "completed!"
+    echo "Start [[${task}]]"
+    ./kata_monitor.sh ${task} &
+    ssh root@127.16.0.2 "export PATH=/usr/local/bin:$PATH; cd /yolo_script; ./smallrun.sh kata ${task}"
 
     sleep 10   
 done
